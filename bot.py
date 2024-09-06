01b0cfc7ae72f208211393afce5b682d30b0c170
Pearoo <sixtyny@gmail.com>
2024-09-06 22:16:11 +0200

    Added channel and role utilities

--- bot.py
+++ bot.py
@@ -6,47 +6,73 @@ import asyncio
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("copy "), intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("copier "), intents=discord.Intents.all())
 
 class Help(commands.HelpCommand):
     async def send_bot_help(self, mapping):
         ctx = self.context
         embed = discord.Embed(
             title="Help",
-            description="This bot is used to clone categories and their channels.",
+            description="Use `copier help [command]` for more information on a command.",
             color=discord.Color.blurple()
         )
-        embed.add_field(
-            name="Commands",
-            value="`copy category [roles] [new_name]` - Clones the current category and all its channels.\n\n`copy delete` - Deletes the current category and all its channels.\n\n`copy nuke` - Deletes the current channel and clones it."
+        for cog, commands in mapping.items():
+            for command in commands:
+                if command.hidden:
+                    continue
+                embed.add_field(
+                    name=ctx.prefix + command.qualified_name,
+                    value=command.short_doc,
+                    inline=False
+                )
+        await ctx.send(embed=embed)
+    
+    async def send_group_help(self, group: commands.Group):
+        ctx = self.context
+        embed = discord.Embed(
+            title=group.name.capitalize(),
+            description=group.help,
+            color=discord.Color.blurple()
         )
+        for command in group.commands:
+            if command.hidden:
+                continue
+            embed.add_field(
+                name=ctx.prefix + (command.usage if command.usage else command.qualified_name),
+                value=command.short_doc,
+                inline=False
+            )
         await ctx.send(embed=embed)
     
     async def send_command_help(self, command: commands.Command):
         ctx = self.context
         embed = discord.Embed(
-            title=command.usage if command.usage else "copy " + command.name,
+            title=command.usage if command.usage else ctx.prefix + command.qualified_name,
             description=command.help,
             color=discord.Color.blurple()
         )
         await ctx.send(embed=embed)
 
-bot.help_command = Help()
-
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f"Logged in as {bot.user} with {len(bot.commands)} commands.")
+
+@bot.group(name="category")
+async def category(ctx: commands.Context):
+    """Utilities for categories. Because Discord is too lazy to add a clone feature."""
+    if ctx.invoked_subcommand is None:
+        await ctx.send_help("category")
 
 @commands.has_permissions(manage_channels=True)
-@bot.command(usage="copy category [roles] [new_name]")
-async def category(ctx: commands.Context, roles: commands.Greedy[discord.Role] = None, *, new_name: str = None):
+@category.command(name="clone", usage="category clone [roles] [new_name]", aliases=["copy"])
+async def category_clone(ctx: commands.Context, roles: commands.Greedy[discord.Role] = None, *, new_name: str = None):
     """Clones the current category and all its channels.
     
     `roles` - Optional. A list of roles to give access to the new category. Seperate them with spaces.
     
     `new_name` - Optional. The name of the new category.
     
-    -# Note: The category will be private if you specify any roles, but visually, the switch for 'private category' will be off. This is a discord limitation, but the category will still be private.
+    **Aliases:** `copy`, `clone`
     """
     parent: discord.CategoryChannel = ctx.channel.category
     if parent is None:
@@ -65,11 +91,13 @@ async def category(ctx: commands.Context, roles: commands.Greedy[discord.Role] =
         }
     name = new_name or parent.name
 
-    category: discord.CategoryChannel = await ctx.channel.category.clone(
+    category: discord.CategoryChannel = await parent.clone(
         name=name,
-        reason=f"Copy {ctx.channel.category.name} - {ctx.author.id}"
+        reason=f"Copy {parent.name} - {ctx.author.id}"
     )
     await category.edit(overwrites=overwrites)
+    
+    await category.move(after=parent, sync_permissions=True)
     
     for channel in parent.channels:
         channel: discord.abc.GuildChannel
@@ -81,7 +109,7 @@ async def category(ctx: commands.Context, roles: commands.Greedy[discord.Role] =
             discord.ForumChannel: "📚"
         }
         
-        cloned = await channel.clone(reason=f"Copy {ctx.channel.category.name} / {emojis[type(channel)]} {channel.name} | {ctx.author.id}")
+        cloned = await channel.clone(reason=f"Copy {parent.name} / {emojis[type(channel)]} {channel.name} | {ctx.author.id}")
         await msg.edit(content=f"Created channel {cloned.mention}...")
         await cloned.move(category=category, end=True, sync_permissions=True)
         await msg.edit(content=f"Moved channel {cloned.mention}...")
@@ -91,8 +119,8 @@ async def category(ctx: commands.Context, roles: commands.Greedy[discord.Role] =
     await ctx.message.delete()
 
 @commands.has_permissions(manage_channels=True)
-@bot.command()
-async def delete(ctx: commands.Context):
+@category.command(name="delete")
+async def category_delete(ctx: commands.Context):
     """Deletes the category that the command is used in, and all its channels."""
     category: discord.CategoryChannel = ctx.channel.category
     if category is None:
@@ -101,16 +129,158 @@ async def delete(ctx: commands.Context):
     for i in category.channels:
         await i.delete(reason=f"Delete {category.name} - {ctx.author.id}")
     await category.delete(reason=f"Delete {category.name} - {ctx.author.id}")
-    await ctx.author.send(f"Deleted category `{category.name}`.")
 
 @commands.has_permissions(manage_channels=True)
-@bot.command()
-async def nuke(ctx: commands.Context):
-    """Deletes the current channel and clones it."""
+@category.command(name="nuke")
+async def category_nuke(ctx: commands.Context):
+    """A shorthand for deleting and cloning a category."""
+    await ctx.invoke(category_clone)
+    await ctx.invoke(category_delete)
     
-    cloned = await ctx.channel.clone(reason=f"Nuke channel - {ctx.author.id}")
-    await cloned.move(category=ctx.channel.category, before=ctx.channel)
-    await ctx.channel.delete(reason=f"Nuke channel - {ctx.author.id}")
-    await cloned.send(f"Channel nuked by {ctx.author.mention}.", allowed_mentions=discord.AllowedMentions.none())
+@bot.group(name="channel")
+async def channel(ctx: commands.Context):
+    """Utilities for channels."""
+    if ctx.invoked_subcommand is None:
+        await ctx.send_help("channel")
+
+@commands.has_permissions(manage_channels=True)
+@channel.command(name="clone", usage="channel clone [roles] [new_name]", aliases=["copy"])
+async def channel_clone(ctx: commands.Context, roles: commands.Greedy[discord.Role] = None, *, new_name: str = None):
+    """Clones the current channel.
+    
+    `roles` - Optional. A list of roles to give access to the new channel. Seperate them with spaces.
+    
+    `new_name` - Optional. The name of the new channel.
+    
+    **Aliases:** `copy`, `clone`
+    """
+    channel: discord.abc.GuildChannel = ctx.channel
+    overwrites = channel.overwrites
+    if roles:
+        overwrites = {
+            role: discord.PermissionOverwrite(
+                read_messages=True,
+                connect=True
+            )
+            for role in roles
+        }
+
+    cloned = await channel.clone(
+        name=new_name or channel.name,
+        reason=f"Copy {channel.name} - {ctx.author.id}"
+    )
+    await cloned.edit(overwrites=overwrites)
+    
+    await ctx.send(f"Channel `{cloned.name}` created. {cloned.mention}")
+
+@commands.has_permissions(manage_channels=True)
+@channel.command(name="delete", usage="channel delete [channel]")
+async def channel_delete(ctx: commands.Context, channel: discord.TextChannel = None):
+    """Deletes the current channel or the specified channel.
+    
+    `channel` - Optional. The channel to delete. Can be a channel ID.
+    """
+    channel = channel or ctx.channel
+    try:
+        channel = await commands.TextChannelConverter().convert(ctx, str(channel))
+    except commands.BadArgument:
+        return await ctx.send("Channel not found.")
+    
+    await channel.delete(reason=f"Delete channel - {ctx.author.id}")
+
+@commands.has_permissions(manage_channels=True)
+@channel.command(name="nuke")
+async def channel_nuke(ctx: commands.Context):
+    """A shorthand for cloning and deleting a channel."""
+    await ctx.invoke(channel_clone)
+    await ctx.invoke(channel_delete)
+
+@bot.group(name="role")
+async def role(ctx: commands.Context):
+    """Utilities for roles. Because Discord is too lazy to add a clone feature to them."""
+    if ctx.invoked_subcommand is None:
+        await ctx.send_help("role")
+
+@commands.has_permissions(manage_roles=True)
+@role.command(usage="role clone [role] [new_color] [new_name]", aliases=["copy"])
+async def role_clone(ctx: commands.Context, role: discord.Role, new_color: str = None, *, new_name: str = None):
+    """Clones a role.
+    
+    `role` - The role to clone. Can be a role ID.
+    
+    `new_color` - Optional. The color of the new role. Must be a hex code and must start with a `#`.
+    
+    `new_name` - Optional. The name of the new role.
+    
+    **Aliases:** `copy`, `clone`
+    """
+    try:
+        role = await commands.RoleConverter().convert(ctx, str(role))
+    except commands.BadArgument:
+        return await ctx.send("Role not found.")
+    if role.is_default():
+        return await ctx.send("You cannot copy the everyone role.")
+    if role.is_premium_subscriber():
+        return await ctx.send("You cannot copy the premium subscriber role.")
+    
+    if new_color and not new_color.startswith("#"):
+        new_name = new_color + " " + new_name if new_name else new_color if new_color else None
+        new_color = None
+    elif not new_color:
+        new_color = None
+    elif new_color and new_color.startswith("#"):
+        try:
+            new_color = discord.Color.from_str(new_color)
+        except ValueError:
+            return await ctx.send("Invalid color. Must be a hex code.")
+    print(new_color, new_name, role.color)
+    new_role = await ctx.guild.create_role(
+        name=new_name or role.name,
+        color=new_color or role.color,
+        permissions=role.permissions,
+        hoist=role.hoist,
+        mentionable=role.mentionable,
+        reason=f"Copy {role.name} - {ctx.author.id}"
+    )
+    extras = ""
+    bot_profile = await ctx.guild.fetch_member(bot.user.id)
+    if role.position < bot_profile.top_role.position:
+        new_role = await new_role.edit(position=role.position - 1)
+    else:
+        extras = "I couldn't move it below the existing role, though, because it is above my top role. You can find it below my top role."
+        new_role = await new_role.edit(position=bot_profile.top_role.position - 1)
+    
+    await ctx.send(f"Role `{new_role.name}` created. {extras}\n{new_role.mention}")
+
+@commands.has_permissions(manage_roles=True)
+@role.command(name="delete")
+async def role_delete(ctx: commands.Context, role: discord.Role):
+    """Deletes a role.
+    
+    `role` - The role to delete. Can be a role ID.
+    """
+    try:
+        role = await commands.RoleConverter().convert(ctx, str(role))
+    except commands.BadArgument:
+        return await ctx.send("Role not found.")
+    if role.is_default():
+        return await ctx.send("You cannot delete the everyone role.")
+    if role.is_premium_subscriber():
+        return await ctx.send("You cannot delete the premium subscriber role.")
+    
+    await role.delete(reason=f"Delete {role.name} - {ctx.author.id}")
+    await ctx.send(f"Role `{role.name}` deleted.")
+
+@commands.has_permissions(manage_roles=True)
+@role.command(name="nuke")
+async def role_nuke(ctx: commands.Context, role: discord.Role):
+    """Nukes a role by cloning it and deleting the original. This should be used if you want to remove a role from everyone.
+    
+    `role` - The role to nuke. Can be a role ID.
+    """
+    await ctx.invoke(role_clone, role)
+    await ctx.invoke(role_delete, role)
+
+bot.help_command = Help()
 
 bot.run(os.getenv("TOKEN"))
